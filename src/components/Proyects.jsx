ebbfa3d7ad8c56e033f8cf01ea0e4da7eeeb8a79
Adro Galvan <adrogdek@gmail.com>
2023-02-16 16:48:48 -0300

    fix: paddings-margins-colours

---
+++ src/components/Proyects.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Klogo from '../assets/klogo'
+
+const Proyects = () => {
+  return (
+    <div>
+        <div className="flex justify-between items-center max-w-[1280px] mx-auto text-[#C3FF00] px-10 mb-32 mt-32">
+        <Klogo />
+        <a className="hidden md:flex text-2xl" href="">
+        {" <contact/> "}
+        </a>
+        </div>
+    </div>
+  )
+}
+
+export default Proyects
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -16,26 +16,29 @@ const Services = () => {
         <div className='flex items-center justify-center container mx-auto p-10'>
         <div className='grid grid-cols-1 mg:grid-cols-2 lg:grid-cols-3 gap-10'>
             <div>
-                <div className='p-5 flex flex-col bg-[#121315] rounded-xl'>
+                <div className='p-9 flex flex-col bg-[#121315] rounded-xl'>
                     <h5 className='text-[#FF0073] text-2xl font-bold mt-3 mb-10'> {'<Desarrolo Web/>'}</h5>
-                    <p className='text-[#dcdcdc] text-lg '> Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, debitis. Illo, ullam cum molestias, eos recus</p>
+                    <p className='text-[#dcdcdc] text-base  '> Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, debitis. Illo, ullam cum molestias, eos recus</p>
                 </div>
             </div>
 
             <div>
-                <div className='p-5 flex flex-col bg-[#121315] rounded-xl'>
-                    <h5 className='text-[#FF0073] text-2xl font-bold mt-3 mb-10'> {'<Desarrolo de aplicaciones/>'}</h5>
-                    <p className='text-[#dcdcdc] text-lg '> Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, debitis. Illo, ullam cum molestias, eos recus</p>
+                <div className='p-9 flex flex-col bg-[#121315] rounded-xl'>
+                    <h5 className='text-[#FF0073] text-2xl font-bold mb-5'> {'<Desarrolo de aplicaciones/>'}</h5>
+                    <p className='text-[#dcdcdc] text-base '> Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, debitis. Illo, ullam cum molestias, eos recus</p>
                 </div>
             </div>
 
             <div>
-                <div className='p-5 flex flex-col bg-[#121315] rounded-xl '>
+                <div className='p-9 flex flex-col bg-[#121315] rounded-xl'>
                     <h5 className='text-[#FF0073] text-2xl font-bold mt-3 mb-10'> {'<Desarrolo mobile/>'}</h5>
-                    <p className='text-[#dcdcdc] text-lg '> Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, debitis. Illo, ullam cum molestias, eos recus</p>
+                    <p className='text-[#dcdcdc] text-base'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, debitis. Illo, ullam cum molestias, eos recus</p>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="pepe">
+        <h3 className='flex text-xl text-bold text-[#C3FF00] ml-28 >'> {'<Services/>'} </h3>
     </div>
     </div>
   )
