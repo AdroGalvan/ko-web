29b29602722291144e8b6d1d731d726c17a5709d
Adro Galvan <adrogdek@gmail.com>
2023-02-16 14:05:23 -0300

    feat: add cards-logos-background

---
+++ src/assets/klogo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Klogo = () => {
+  return (
+    <svg width="36" height="57" viewBox="0 0 105 165" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M58.8479 0L19.8023 164.275H0L39.0455 0H58.8479Z" fill="#C3FF00"/>
+    <path d="M78.9533 118.683L47.4326 86.0675L78.9533 53.4519H104.393L72.8728 86.0675L104.393 118.683H78.9533Z" fill="#C3FF00"/>
+    </svg>
+  )
+}
+
+export default Klogo
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import Logo from '../assets/kodevslogo'
-import {AiOutlineMail} from 'react-icons/ai'
+import React from "react";
+import Logo from "../assets/kodevslogo";
+import { AiOutlineMail } from "react-icons/ai";
 
 const Navbar = () => {
   return (
-    <div className='mt-24 flex h-24 justify-between items-center max-w-[1280px] mx-auto  text-[#C3FF00] px-10'>
-        <Logo />
-        <a className='hidden md:flex text-2xl' href=""> Contacto </a>
-        <div className='items-center block md:hidden'>
-            <AiOutlineMail size={40} />
-        </div>
+    <div className="mt-16 flex h-24 justify-between items-center max-w-[1280px] mx-auto  text-[#C3FF00] px-10">
+      <Logo />
+      <a className="hidden md:flex text-2xl" href="">
+        {" <contact/> "}
+      </a>
+      <div className="items-center block md:hidden">
+        <a href="">
+          {" "}
+          <AiOutlineMail size={40} />{" "}
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
